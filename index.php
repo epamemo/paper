796192e66d892f17846291ca654225e79b5dd73e
Epafraditus Memoriano <epamemo@gmail.com>
2020-07-28 11:41:39 +0700

    edit the index page and css

--- index.php
+++ index.php
@@ -1,44 +1,9 @@
 <?php
-include 'config.php';
-date_default_timezone_set('Asia/Jakarta');
-
-if (isset($_POST['newMessage'])) {
-    $nama = $_POST['nama'];
-    $pesan = $_POST['pesan'];
-    $tanggal = $_POST['tanggal'];
-    mysqli_query($mysqli, "INSERT INTO pesan VALUES('', '$nama', '$pesan', '$tanggal')");
-
-    header("location:thanks.html");
-}
-
+include 'header.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Paper</title>
-</head>
-
-<body>
-    <div id="paper">
-        <div id="line">
-            <div id="content">
-                <p>Test</p>
-                <form action="index.php" method="POST">
-                    <textarea name="pesan" id="pesan" placeholder="tuliskan pesanmu disini"></textarea>
-                    <label for="pengirim">from: </label>
-                    <input type="text" name="nama" id="nama" placeholder="nama pengirim"><br>
-                    <input type="hidden" name="tanggal" value="<?php echo date('d-m-Y H:i:s'); ?>">
-                    <button type="submit" name="newMessage">Send</button>
-                    <!-- <input type="submit" name="newMessage" value="new"> -->
-                </form>
-            </div>
-        </div>
-    </div>
-</body>
-
-</html>
+<img class="ilustrasi" src="img/02.png" alt="illustration from absurd.design">
+<p>Hai, kamu bisa mulai tulis pesan dengan klik link dibawah ya!</p>
+<a href="pesan.php">Tulis Pesan</a>
+<?php
+include 'footer.php';
+?>
